a894eb093de1f3e2cfc58c42d8a52a22e4c35a90
Bradley Coesens <bcoesen1@ford.com>
2024-05-05 17:16:32 -0400

    End of tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HeroesComponent} from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tour-of-heros';
+  title = 'Tour of Heroes';
 }
